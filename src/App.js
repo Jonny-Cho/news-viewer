3311a1f8ddae127038171622cc8e9843239c668d
Jonny-Cho <andante2183@gmail.com>
2020-07-14 23:16:33 +0900

    Add news viewer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import axios from 'axios'
+import NewsList from './components/NewsList'
 
 const App = () => {
+  return <NewsList />
 }
 
 export default App
